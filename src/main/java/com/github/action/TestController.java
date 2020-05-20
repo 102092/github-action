876622ddd28b90601173fa4b5695a53c61f8115a
Han <quaint3@gmail.com>
2020-05-20 23:21:26 +0900

    deploy

---
+++ src/main/java/com/github/action/TestController.java
@@ -0,0 +1,15 @@
+package com.github.action;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+  @GetMapping("/test")
+  public String test() {
+    String testStr = "deploy complete";
+//    System.out.println(testStr);
+    return testStr;
+  }
+}
