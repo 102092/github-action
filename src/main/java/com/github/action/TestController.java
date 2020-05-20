7c3e1239f9d608b67f6acf0d5126a4ce1b23c725
Han <quaint3@gmail.com>
2020-05-20 23:27:09 +0900

    deploy

--- src/main/java/com/github/action/TestController.java
+++ src/main/java/com/github/action/TestController.java
@@ -8,7 +8,7 @@ public class TestController {
 
   @GetMapping("/test")
   public String test() {
-    String testStr = "deploy complete";
+    String testStr = "deploy complete!!";
 //    System.out.println(testStr);
     return testStr;
   }
